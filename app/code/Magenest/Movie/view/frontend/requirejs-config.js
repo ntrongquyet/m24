61f5a3f284dcfa8282f7f9f7e9f32255493ed15d
ntrongquyet <ntrongquyet37@gmail.com>
2021-03-29 09:23:41 +0700

    mixins checkout

--- app/code/Magenest/Movie/view/frontend/requirejs-config.js
+++ app/code/Magenest/Movie/view/frontend/requirejs-config.js
@@ -9,6 +9,10 @@ var config = {
         mixins: {
             'Magento_Checkout/js/action/set-shipping-information': {
                 'Magenest_Movie/js/action/set-shipping-information-mixin': true
+            },
+            'Magento_Checkout/js/model/step-navigator': {
+                'Magenest_Movie/js/model/step-navigator-mixin': true
+
             }
         }
     }
